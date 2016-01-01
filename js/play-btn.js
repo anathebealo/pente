ae55b29b405bc9e7515d5fdf4b0a083128b38d6f
Chris Hanna <channa@aptima.com>
2016-01-01 15:01:34 -0500

    adds cap boxes

--- js/play-btn.js
+++ js/play-btn.js
@@ -5,13 +5,13 @@ playBtn
 	.style('cursor', 'pointer')
 	.on('mouseover', function(){
 		d3.select(this).transition()
-			.style('background-color', 'red')
+			.style('background-color', '#3261FC')
 			.style('width', 420)
 			.ease('cubic-in')
 	})
 	.on('mouseout', function(){
 		d3.select(this).transition()
-			.style('background-color', '#FF4B4B')
+			.style('background-color', '#4B75FF')
 			.style('width', 400)
 			.ease('cubic-out')
 	})
@@ -35,12 +35,47 @@ playBtn
 			d3.select('#playBtn').transition()
 				.text('Reset')
 
-			d3.selectAll('.cap-area').transition()
+			function addCapContent(plr, text){
+				d3.select(plr)
+				.append('h2')
+				.text(text)		
+				.attr('class', 'capture-text')
+				.style('opacity', '0')
+				.transition()
+				.duration(700)
+				.ease('cubic-out')
+				.style('opacity', '1')	
+				.transition()
+			}
+
+			var plrCaps = d3.selectAll('.cap-area').transition()
 				.duration(700)
 				.ease('cubic-out')
 				.style('height', boardSize + boardOffset*2)
 				.transition()
-				.style('width', '150px')
+				.style('width', '250px')
+				.style('border-style', 'solid')
+				.style('border-width', '2px')
+				.style('border-color', 'gray')
+				.style('margin', '0px 50px')
+				.each('end', function(){
+					d3.select('.cap-area.first').transition()
+						.duration(800)
+						.ease('cubic-out')
+						.style('background-color', '#7AFF6C')
+						.each('end', function(){
+							addCapContent(this, 'Player 1 Captures')
+						})
+
+					d3.select('.cap-area.second').transition()
+						.duration(800)
+						.ease('cubic-out')
+						.style('background-color', '#FF6C6C')
+						.each('end', function(){
+							addCapContent(this, 'Player 2 Captures')
+						})
+
+				})
 
 			firstPlay = false;
 		} else {
