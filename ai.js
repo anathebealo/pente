ada915f7b089b00b5ff324a063ad3792935a7a07
Anastasia Bealo <abealo@u.rochester.edu>
2016-01-01 16:22:50 -0500

    changed all of ai.js to use gameState instead of board array -- new function names, watch out

--- ai.js
+++ ai.js
@@ -22,39 +22,37 @@ AI is always player 2 **
 	Driver function to find next move
 */	
 function find_next_move(gameState, depth) {
-	var new_board = max_value(gameState.board, depth).board_state;
-	console.log("new board");
-	print_board(new_board);
-	return new_board; 
+	var new_game = max_value(gameState, depth).game_state;
+	return new_game; 
 }
 
 /*
 	Function contributing to minmax algorithm 
 	max looks to maximize player 2's moves
 */
-function max_value(board, depth) {
+function max_value(gameState, depth) {
 	if( depth == 0 ) {
 		var ret_val = {
-			heuristic_val: heuristic(board, AI_MARKER),
-			board_state: board
+			heuristic_val: heuristic(gameState, AI_MARKER),
+			game_state: gameState
 		};
 		return ret_val;
 	} else {
 		var alpha = Number.NEGATIVE_INFINITY; 
 		var best_board = undefined;
-		var all_boards = find_all_moves_on_board(board, AI_MARKER);
+		var all_games = find_all_moves_on_board(gameState, AI_MARKER);
 		
-		all_boards.forEach(function(next_board){
-			var min_val = min_value(next_board, depth -1);
+		all_games.forEach(function(next_game){
+			var min_val = min_value(gameState, depth -1);
 			if (min_val.heuristic_val > alpha){
 				alpha = min_val.heuristic_val;
-				best_board = copy_array(next_board);
+				best_game = copy_game(next_game);
 			}
 		});
 
 		return {
 			heuristic_val: alpha,
-			board_state: best_board
+			game_state: best_game
 		};
 	}
 }
@@ -63,29 +61,29 @@ function max_value(board, depth) {
 	Function contributing to minmax algorithm 
 	min looks to maximize player 1's moves
 */
-function min_value(board, depth) {
+function min_value(gameState, depth) {
 	if( depth == 0 ) {
 		var ret_val = {
-			heuristic_val: heuristic(board, HUMAN_MARKER),
-			board_state: board
+			heuristic_val: heuristic(gameState, HUMAN_MARKER),
+			game_state: gameState
 		};
 		return ret_val;
 	} else {
 		var beta = Infinity;
-		var best_board = undefined;
-		var all_boards = find_all_moves_on_board(board, HUMAN_MARKER);
+		var best_game = undefined;
+		var all_games = find_all_moves_on_board(gameState, HUMAN_MARKER);
 		
-		all_boards.forEach(function(next_board){
-			var max_val = max_value(next_board, depth - 1);
+		all_games.forEach(function(next_game){
+			var max_val = max_value(next_game, depth - 1);
 			if (max_val.heuristic_val < beta){
 				beta = max_val.heuristic_val;
-				best_board = copy_array(next_board);
+				best_game = copy_game(next_game);
 			}
 		});
 
 		return {
 			heuristic_val: beta,
-			board_state: best_board
+			game_state: best_game
 		};
 	}
 }
@@ -93,36 +91,41 @@ function min_value(board, depth) {
 /*
 	Finds all moves that a player can make on a given board state. Returns a list of all of them.
 */
-function find_all_moves_on_board(board, player_num) {
-	var board_states = [];
+function find_all_moves_on_board(gameState, player_num) {
+	var game_states = [];
 	var count = 0;
-	for( var i = 0; i<board.length; i++ ) {
-		for( var j = 0; j<board[i].length; j++ ) {
-			if( board[i][j] == EMPTY_MARKER ) {
-				var next_state = copy_array(board);
-				make_move(next_state, player_num, i, j);
-				board_states.push(copy_array(next_state));
+	for( var i = 0; i<gameState.board.length; i++ ) {
+		for( var j = 0; j<gameState.board[i].length; j++ ) {
+			if( gameState.board[i][j] == EMPTY_MARKER ) {
+				var next_game = copy_game(gameState);
+				make_move(next_game, player_num, i, j);
+				game_states.push(copy_game(next_game));
 			}
 		} 
 	}
-	return board_states;
+	return game_states;
 }
 
 /*
 	This makes a move on a given board state -- if the move made is a capture, it updates for that as well.
 	Returns the board state
 */
-function make_move(board, player_num, row, col) {
+function make_move(gameState, player_num, row, col) {
 	for( var i = -1; i<2; i++ ) {
 		for( var j = -1; j<2; j++ ) {
 			if( i != 0 || j != 0 ) {
 				//if there are two pieces of the other player's in a row and then your own piece, then update to reflect the capture.
-				if( count_other_player_in_row(board, row, col, i, j, player_num) == 2 ) {
-					if( row + 3*i > -1 && row + 3*i < board.length &&
-						col + 3*j > -1 && col + 3*j < board[row].length) {
-						if( board[row + 3*i][col + 3*j] == player_num ) {
-							board[row + 2*i][col + 2*j] = EMPTY_MARKER;
-							board[row + i][col + j] = EMPTY_MARKER;
+				if( count_other_player_in_row(gameState.board, row, col, i, j, player_num) == 2 ) {
+					if( row + 3*i > -1 && row + 3*i < gameState.board.length &&
+						col + 3*j > -1 && col + 3*j < gameState.board[row].length) {
+						if( gameState.board[row + 3*i][col + 3*j] == player_num ) {
+							gameState.board[row + 2*i][col + 2*j] = EMPTY_MARKER;
+							gameState.board[row + i][col + j] = EMPTY_MARKER;
+							if(player_num == 1) {
+								gameState.human_captures++;
+							} else {
+								gameState.ai_captures++;
+							}
 						}
 					}
 				}
@@ -130,8 +133,8 @@ function make_move(board, player_num, row, col) {
 		}
 	}
 
-	board[row][col] = player_num;
-	return board;
+	gameState.board[row][col] = player_num;
+	return gameState;
 }
 
 /*
@@ -177,13 +180,17 @@ function count_other_player_in_row(board, row, col, i, j, player_num) {
 	This returns a deep copy of the game board to prevent updating the 'live' board version
 	Returns the new board
 */
-function copy_array(board) {
+function copy_game(gameState) {
 	var new_array = [];
-	for( var i = 0; i<board.length; i++ ) {
-		new_array.push(board[i].slice());
+	for( var i = 0; i<gameState.board.length; i++ ) {
+		new_array.push(gameState.board[i].slice());
 	}
 
-	return new_array;
+	return { 
+		board: new_array,
+		ai_captures: gameState.ai_captures,
+		human_captures: gameState.human_captures
+	};
 }
 
 
@@ -191,13 +198,13 @@ function copy_array(board) {
 	This function outputs a value for the current state of the board. 
 	Higher return values means better board state for the whichever player for whom it is scored.
 */
-function heuristic(board, player_num) {
+function heuristic(gameState, player_num) {
 	// TODO: THIS IS DUMB -- MAKE IT BETTER PLEASEEE
 
 	var ret_val = 0;
-	if(board[0][0] == 2) {
+	if(gameState.board[0][0] == 2) {
 		return 100; 
-	} else if(board[0][2] == 2) {
+	} else if(gameState.board[0][2] == 2) {
 		return 99;
 	} else {
 		ret_val = -100;
@@ -209,15 +216,17 @@ function heuristic(board, player_num) {
 /*
 	Prints the gameboard to console 
 */
-function print_board(board){
+function print_game(gameState){
 	var string_to_print = ""; 
-	for( var i = 0; i<board.length; i++ ) {
-		for( var j = 0; j<board[i].length; j++ ) {
-			string_to_print += board[j][i] + ", "; 
+	for( var i = 0; i<gameState.board.length; i++ ) {
+		for( var j = 0; j<gameState.board[i].length; j++ ) {
+			string_to_print += gameState.board[j][i] + ", "; 
 		} 
 		console.log(i + ": " + string_to_print)
 		string_to_print = ""; 
 	}
+	console.log("Human Captures: " + gameState.human_captures);
+	console.log("AI Captures: " + gameState.ai_captures);
 }
 
 
